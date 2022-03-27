eeff9471cdbf7f3b64959a884c8db6da30ea4e41
bp00048 <bp00048@cs6242-vm02.uwg.westga.edu>
2022-03-27 16:52:33 -0400

    add if/else statement for showing % of yes votes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,5 +4,15 @@ class Restaurant < ApplicationRecord
     with:     %r{\.(com|net|couk|edu)\z}i,
     message: 'Must be a valid website.'
   }
+  after_initialize :set_defaults, unless: :persisted?
+
+  validates :yes_split, :no_split, numericality: {
+    greater_than_or_equal_to: 0
+  }
+
+  def set_defaults
+    self.yes_split ||= 0
+    self.no_split ||= 0
+  end
 
 end
