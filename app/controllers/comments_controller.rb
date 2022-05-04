d354d1a26bb1174f9a77abe2ac0d7e8d43cc4c19
bp00048 <bp00048@cs6242-vm04.uwg.westga.edu>
2022-05-04 15:28:50 -0400

    ability to create comment if logged in

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = current_user.comments.new(comment_params)
+    if !@comment.save
+      flash[:notice] = @comment.errors.full_messages.to_sentence
+    end
+    redirect_to restaurant_path(params[:restaurant_id])
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(restaurant_id: params[:restaurant_id])
+  end
 end
